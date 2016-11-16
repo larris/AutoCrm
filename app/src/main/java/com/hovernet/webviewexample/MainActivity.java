1aa712855aebd6906804a3dd5a9d0121e01cc8e4
larris <fantidisl@gmail.com>
2016-11-16 14:03:42 +0200

    implement cordova

--- app/src/main/java/com/hovernet/webviewexample/MainActivity.java
+++ app/src/main/java/com/hovernet/webviewexample/MainActivity.java
@@ -1,36 +1,51 @@
 package com.hovernet.webviewexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+import org.apache.cordova.DroidGap;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DroidGap {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.myWebView);
-
-
-        myWebView.setWebViewClient(new WebViewClient());
-
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.getSettings().setDomStorageEnabled(true);
+        super.init();
+        super.clearCache();
 
         String urlString = getIntent().getStringExtra("urlString");
 
-        myWebView.loadUrl(urlString);
+        super.loadUrl(urlString);
     }
 
+    public void onReceivedError(int errorCode, String description, String failingUrl) {
+        super.loadUrl("file:///android_asset/www/noconnection.html");
+        return;
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        return;
+
+    }
 
 }
-
+//    OLD ACTIVITY
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        WebView myWebView = (WebView) findViewById(R.id.myWebView);
+//
+//
+//        myWebView.setWebViewClient(new WebViewClient());
+//
+//        myWebView.setWebViewClient(new WebViewClient());
+//        myWebView.getSettings().setJavaScriptEnabled(true);
+//        myWebView.getSettings().setDomStorageEnabled(true);
+//
+//        String urlString = getIntent().getStringExtra("urlString");
+//
+//        myWebView.loadUrl(urlString);
+//    }
